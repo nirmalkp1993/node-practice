f1a893229623ad63e219b734c21b68ca0e816ba7
nirmalkp1993 <nirmal.prajapat@qdegrees.com>
2024-02-12 23:13:41 +0530

    send html response

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Hello Nirmal</title></head>");
+  res.write("<body><h1>My First Node Js Server</h1></body>");
+  res.write("</html>");
+  res.end();
 });
-server.listen(8000);
+server.listen(8400);
