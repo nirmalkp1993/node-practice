dc4fc5890aa90fb5c9c2502ced4a0c6de94741db
nirmalkp1993 <nirmal.prajapat@qdegrees.com>
2024-02-13 00:00:32 +0530

    redirecting request

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Hello Nirmal</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type'text' name='message'></input><button type='submit'>Submit</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "dummy tetx");
+    res.statusCode === 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>Hello Nirmal</title></head>");
-  res.write("<body><h1>My First Node Js Server</h1></body>");
+  res.write("<body><h1></h1></body>");
   res.write("</html>");
   res.end();
 });
