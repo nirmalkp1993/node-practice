abbfac48f2b7ca7865bc8867af5731be52a71c32
nirmalkp1993 <nirmal.prajapat@qdegrees.com>
2024-02-14 00:10:41 +0530

    parsing request body

--- app.js
+++ app.js
@@ -14,7 +14,18 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "dummy tetx");
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+      console.log(parsedBody);
+    });
+
     res.statusCode === 302;
     res.setHeader("Location", "/");
     return res.end();
